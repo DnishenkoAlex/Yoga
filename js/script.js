dd5165bd600f545657ca1f3e4698b451be28c423
DnishenkoAlex <remax.od@gmail.com>
2020-07-17 12:50:16 +0300

    yoga_timer+modal

--- js/script.js
+++ js/script.js
@@ -112,5 +112,16 @@ window.addEventListener('DOMContentLoaded', function() {
         document.body.style.overflow = '';
     });
 
+    
+{/* <input id="age" value="30"> */}
+var age = document.getElementById('age');
+
+function showUser(surname, name) {
+    alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+}
+showUser.apply(age, ['Днищенко', 'Алексей']);
+
+
+
 });
 
