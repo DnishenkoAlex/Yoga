b83512ebec6887b538f88d43363abfbb9de90bd1
DnishenkoAlex <remax.od@gmail.com>
2020-07-20 14:37:13 +0300

    form

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
-    var tab = document.querySelectorAll('.info-header-tab'),
+    let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
 
     function hideTabContent(a) {
-        for (var i = a; i < tabContent.length; i++) {
+        for (let i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
         }
@@ -22,9 +22,9 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     info.addEventListener('click', function(event) {
-        var target = event.target;
+        let target = event.target;
         if (target && target.classList.contains('info-header-tab')) {
-            for(var i = 0; i < tab.length; i++) {
+            for(let i = 0; i < tab.length; i++) {
                 if (target == tab[i]) {
                     hideTabContent(0);
                     showTabContent(i);
@@ -35,26 +35,15 @@ window.addEventListener('DOMContentLoaded', function() {
 
     });
 
+    // Timer 
 
-    //timer
+    let deadline = '2018-11-21';
 
-    var deadLine = '2020-07-08';
-    
     function getTimeRemaining(endtime) {
-        var t = Date.parse(endtime) - Date.parse(new Date()); // вычисление разницы между сейчас и окончанеим таймера в мили секундах
-        var seconds = Math.floor((t/1000) % 60);
-        var minutes = Math.floor((t/1000/60) % 60);
-        var hours = Math.floor((t/1000/60/60) %  60);
-
-        if (seconds < 10) {
-            seconds = '0' + seconds;
-        }
-        if (minutes < 10) {
-            minutes = '0' + minutes;
-        }
-        if (hours < 10) {
-            hours = '0' + hours;
-        }
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/(1000*60*60)));
 
         return {
             'total' : t,
@@ -63,40 +52,42 @@ window.addEventListener('DOMContentLoaded', function() {
             'seconds' : seconds
         };
     }
-    
 
     function setClock(id, endtime) {
-        var timer = document.getElementById(id);
-        var hours = timer.querySelector('.hours');
-        var minutes = timer.querySelector('.minutes');
-        var seconds = timer.querySelector('.seconds');
-        var timeInterval = setInterval(updataClock, 1000);
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+            
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
 
-        if(hours < 10) {
-            hours = '0' + hours;
-        }
+            function addZero(num){
+                        if(num <= 9) {
+                            return '0' + num;
+                        } else return num;
+                    };
 
-        function updataClock() {
-            var t = getTimeRemaining(endtime);
-            hours.textContent = t.hours;
-            minutes.textContent = t.minutes;
-            seconds.textContent = t.seconds;
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
             if (t.total <= 0) {
                 clearInterval(timeInterval);
                 hours.textContent = '00';
                 minutes.textContent = '00';
                 seconds.textContent = '00';
-                
             }
         }
 
     }
-    
-    setClock('timer', deadLine);
 
-    //modal
+    setClock('timer', deadline);
 
-    var more = document.querySelector('.more'),
+    // Modal
+
+    let more = document.querySelector('.more'),
         overlay = document.querySelector('.overlay'),
         close = document.querySelector('.popup-close');
 
@@ -112,16 +103,52 @@ window.addEventListener('DOMContentLoaded', function() {
         document.body.style.overflow = '';
     });
 
-    
-{/* <input id="age" value="30"> */}
-var age = document.getElementById('age');
+     // Form
 
-function showUser(surname, name) {
-    alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
-}
-showUser.apply(age, ['Днищенко', 'Алексей']);
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'Что-то пошло не так...'
+    };
 
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(form);
+
+        let obj = {};
+        formData.forEach(function(value, key) {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if(request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    });
 
 
 });
-
